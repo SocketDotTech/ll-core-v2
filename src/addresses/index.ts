68a1733f930e490a0abce57c03d222bed03e3e77
Aniket Sharma <aniket965.as@gmail.com>
2023-09-27 16:44:37 +0530

    feat: add stargate receiver on polygon

--- src/addresses/index.ts
+++ src/addresses/index.ts
@@ -473,6 +473,7 @@ export const addresses: {
 
   [ChainId.POLYGON_CHAIN_ID]: {
     socketGateway: "0x3a23F943181408EAC424116Af7b7790c94Cb97a5",
+    stargateReceiver: "0x8DfeB2e0B392f0033C8685E35FB4763d88a70d12",
     routeIds: {
       [Bridge.Hop]: {
         routeId: 22,
